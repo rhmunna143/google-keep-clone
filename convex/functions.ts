4c393a775d6d40e87e4ec41f98142471cc6779cd
rhmunna143 <rhmunna19@gmail.com>
2024-10-15 15:18:59 +0600

    file structure added with convex setup

---
+++ convex/functions.ts
@@ -0,0 +1,17 @@
+import { query, mutation } from "./_generated/server";
+import { v } from "convex/values";
+
+export const getNotes = query({
+  args: {},
+  handler: async (ctx) => {
+    return await ctx.db.query("notes").collect();
+  },
+});
+
+export const addNote = mutation({
+  args: {text: v.string()},
+  handler: async (ctx, args) => {
+    const newData = await ctx.db.insert("notes", { text: args.text });
+    return newData;
+  },
+});
