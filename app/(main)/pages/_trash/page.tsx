3e8f2f0215dcca1dbbc1817e61d0309d863b2d5e
rhmunna143 <rhmunna19@gmail.com>
2024-10-18 16:29:44 +0600

    work save

---
+++ app/(main)/pages/_trash/page.tsx
@@ -0,0 +1,35 @@
+// "use client";
+
+// import { useQuery } from "convex/react";
+// import React from "react";
+
+// interface Note {
+//   _id: string;
+//   title?: string;
+//   text?: string;
+//   _creationTime: string;
+// }
+
+// interface TrashPageProps {
+//   note: Note;
+// }
+
+// const TrashPage: React.FC<TrashPageProps> = () => {
+//   const trashNotes = useQuery(getDeletedNotes);
+
+//   return (
+//     <div className="p-4">
+//       {trashNotes?.map((note: Note) => (
+//         <div key={note._id} className="border p-4 bg-gray-50">
+//           {note.text}
+//           <div className="flex justify-end space-x-2">
+//             <button className="text-green-500">Restore</button>
+//             <button className="text-red-500">Delete Forever</button>
+//           </div>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default TrashPage;
