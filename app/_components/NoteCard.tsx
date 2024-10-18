268d74c2b36e6f5d56baec33a8d6b86c3cf11935
rhmunna143 <rhmunna19@gmail.com>
2024-10-18 12:42:09 +0600

    note card padding fixes

--- app/_components/NoteCard.tsx
+++ app/_components/NoteCard.tsx
@@ -25,17 +25,17 @@ const NoteCard: React.FC<NoteCardProps> = ({ note }) => {
     >
       <input
         defaultValue={note?.title}
-        className="w-full text-lg font-medium p-4 rounded-t-md"
+        className="w-full text-lg font-medium px-4 py-1 rounded-t-md"
       />
 
       <textarea
         defaultValue={note?.text}
-        className="w-full resize-none px-4 py-1 rounded-b-md"
+        className="w-full resize-none px-4 pt-1 rounded-b-md"
       />
 
       {/* Option buttons with visibility/opacity transition */}
       <div
-        className={`absolute right-4 bottom-4 flex space-x-2 transition-opacity duration-200 ${
+        className={`relative right-1 bottom-1 top-1 flex space-x-2 transition-opacity duration-200 ${
           isShowOptions ? "opacity-100 visible" : "opacity-0 invisible"
         }`}
       >
